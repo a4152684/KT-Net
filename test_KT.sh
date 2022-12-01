afd2288545553f1a7a4eea108fe0843963536e78
True C <62787917+a4152684@users.noreply.github.com>
2022-12-01 18:43:41 +0800

    Create test_KT.sh

---
+++ test_KT.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+latent_dim=1024
+epochs=600
+save_frequency=200
+lr=1e-4
+batch_size=32
+dataset_name=3depn
+# dataset_name=crn
+
+for category in plane #cabinet car chair lamp sofa table watercraft
+do
+    sh run/test-KT.sh ${category} ${latent_dim} ${dataset_name}
+done
