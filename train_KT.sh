c5add2c2bca780efd881e8d6ecb6ce04e58e5930
True C <62787917+a4152684@users.noreply.github.com>
2022-12-01 18:43:06 +0800

    Update and rename run_KT.sh to train_KT.sh

--- train_KT.sh
+++ train_KT.sh
@@ -4,11 +4,10 @@ epochs=600
 save_frequency=200
 lr=1e-4
 batch_size=32
-dataset_name=3depn
-# dataset_name=crn
+# dataset_name=3depn
+dataset_name=crn
 
 for category in plane #cabinet car chair lamp sofa table watercraft
 do
     sh run/train-KT.sh ${category} ${epochs} ${save_frequency} ${latent_dim} ${lr} ${dataset_name} ${batch_size}
-    # sh run/test-KT.sh ${category} ${latent_dim} ${dataset_name}
 done
